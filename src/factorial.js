eb9e0287c648b92a2433393c35ad4c0b85c884e7
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2019-11-24 14:38:33 +0400

    factorial test coverage

--- src/factorial.js
+++ src/factorial.js
@@ -1,57 +1,41 @@
-function getDigits(n){
+function getDigitsReversed(number){
     const digits = [];
-    while (n >= 10) {
-        digits.push(n % 10);
-        n = Math.floor(n / 10);
+    while (number >= 10) {
+        digits.push(number % 10);
+        number = Math.floor(number / 10);
     }
-    digits.push(n);
+    digits.push(number);
     return digits;
 }
 
-function schoolMultiply(digits, multiplier){
+function multiply(reversedDigits, multiplier){
     const result = [];
     let next = 0;
-    for (let i=0, ic = digits.length; i<ic; i++){
-        const multiplicationResult = digits[i] * multiplier + next;
+
+    for (let i=0, ic = reversedDigits.length; i<ic; i++){
+        const multiplicationResult = reversedDigits[i] * multiplier + next;
         const newDigit = multiplicationResult % 10;
         next = Math.floor(multiplicationResult / 10);
         result.push(newDigit);
     }
+
     if (next) 
-        result.push(...getDigits(next));
+        result.push(...getDigitsReversed(next));
     
     return result;
 }
 
-function schoolAdd(digits1, digits2){
-    const length = Math.max(digits1.length, digits2.length);
-    const result = [];
-    let next = 0;
-
-    for (let i=0; i<length; i++){
-        const sum = (digits1[i] || 0) + (digits2[i] || 0) + next;
-        result.push(sum % 10);
-        next = Math.floor(sum / 10);
-    }
-
-    if (next)
-        result.push(next);
-
-    return result;
-}
-
-function fac(n){
-    return n > 1 ? n * n -1 : 1;    
-}
-
-function facExt(n){
-    let digits = getDigits(n);
+function factorialExt(n){
+    let digits = getDigitsReversed(n);
     while (n > 1){
-        digits = schoolMultiply(digits, n-1);
+        digits = multiply(digits, n-1);
         n--;
     }
 
     return digits.reverse();
 }
 
-console.log(facExt(25).join(''));
+module.exports = {
+    factorialExt
+}
+
---
+++ src/factorial.spec.js
@@ -0,0 +1,28 @@
+const factorialExt = require('./factorial.js').factorialExt;
+
+describe('Extra Large Factorial', () => {
+
+    const simpleFactorial = (n) => n > 1 ? n * simpleFactorial(n-1) : 1;
+
+    it('#simpleFactorial should calculate factorial correctly', () => {
+        const testNumber = 5;
+        const expectedResult = 120;
+        expect(simpleFactorial(testNumber)).toEqual(expectedResult);
+    });
+
+    it('Factorials of n>20 can\'t be stored even in a 64-bit long integers', () => {
+        const testNumber = 25;
+        const expectedResult = '15511210043330985984000000';
+
+        const result = simpleFactorial(testNumber).toString(10);
+        expect(result).not.toEqual(expectedResult);
+    });
+
+    it('#factorialExt calculates extra large factorial', () => {
+        const testNumber = 25;
+        const expectedResult = '15511210043330985984000000';
+
+        const result = factorialExt(testNumber).join('');
+        expect(result).toEqual(expectedResult);
+    });
+});
